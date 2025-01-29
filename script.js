af3192b2fda19546bcbc40130cea3b59a4312bb8
Andressa Ferreira <andressaferre03@gmail.com>
2025-01-29 10:34:59 -0300

    button functionality

--- script.js
+++ script.js
@@ -1,26 +1,45 @@
-function sortear () {
-  const totalResultados = Number(document.querySelector('#style-number-2').value)
+let naoRepetirNumero = false;
 
-  const primeiroValor = Number(document.querySelector('#primeiro-valor-input').value)
+function podeRepetirNumero(input) {
+  naoRepetirNumero = input.checked;
+}
 
-  const segundoValor = Number(document.querySelector('#segundo-valor-input').value)
+function sortear() {
+  const totalResultados = Number(document.querySelector('#style-number-2').value);
+  const primeiroValor   = Number(document.querySelector('#primeiro-valor-input').value);
+  const segundoValor    = Number(document.querySelector('#segundo-valor-input').value);
 
    //Limpar resultados antigos
-   const elementoHTMLResultValues = document.querySelector('.results-value')
-   elementoHTMLResultValues.innerHTML = ''
-
-  for (let i = 0; i < totalResultados; i++) {
+   const elementoHTMLResultValues = document.querySelector('.results');
+   elementoHTMLResultValues.innerHTML = '';
  
-  const resultado = Math.floor(Math.random() * (segundoValor - primeiroValor + 1)) + primeiroValor
-  console.log(resultado)
+   let listaNumeroSorteados = [];
+   
+  for (let i = 0; i < totalResultados; i++) {
+    // Gerando um número aleatório
+    let resultado = gerarNumero(primeiroValor, segundoValor);
 
-  // Gerar um elemento HTML para o resultado
-  const resultElement = document.createElement('div')
-  resultElement.classList.add('results-value')
-  resultElement.innerText = resultado
+    // Se a configuração podeRepetir for verdadeira, verifica se o número já existe na lista
+    if (naoRepetirNumero && listaNumeroSorteados.includes(resultado)) {
+      while (listaNumeroSorteados.includes(resultado)) {
+        resultado = gerarNumero(primeiroValor, segundoValor);
+      }
+    }
 
-  // Adicionar o elemento criado dentro do HTML existente
-  elementoHTMLResultValues.append(resultElement)
+    // Adiciona esse novo número gerado numa lista de números
+    listaNumeroSorteados.push(resultado);
 
+    // Gerar um elemento HTML para o resultado
+    const resultElement = document.createElement('div');
+    resultElement.classList.add('results-value');
+    resultElement.innerText = resultado;
+
+    // Adicionar o elemento criado dentro do HTML existente
+    elementoHTMLResultValues.append(resultElement);
   }
+}
+
+function gerarNumero (primeiroValor, segundoValor) {
+  const resultado = Math.floor(Math.random() * (segundoValor - primeiroValor + 1)) + primeiroValor
+  return resultado
 }
